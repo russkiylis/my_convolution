0be545edf530cdcfe632e5d3a7e3f5087c3154ed
russkiylis <russkiylis@koshy.ru>
2026-07-16 14:08:56 +0300

    Вроде как допилил генератор нагрузки. Сейчас засуну его в отдельный поток и потом окажется, что всё в лютых багах

--- Qt/myConvolution/loadgenerator.cpp
+++ Qt/myConvolution/loadgenerator.cpp
@@ -1,61 +1,180 @@
+#include <random>
+#include <algorithm>
+#include <iterator>
+
 #include "loadgenerator.h"
 
-LoadGenerator::PostConfig::PostConfig(const PostConfig &other):
+LoadGenerator::PostConfig::PostConfig(const PostConfig &other) :
+    enabled(false),
     postName(other.postName),
     latitude(other.latitude),
     longitude(other.longitude),
     frequency(other.frequency),
     level(other.level),
     levelSigma(other.levelSigma),
-    min_angle(other.min_angle),
-    max_angle(other.max_angle),
-    step(other.step),
+    minAngleH(other.minAngleH),
+    maxAngleH(other.maxAngleH),
+    minAngleV(0),
+    maxAngleV(0),
+    stepH(other.stepH),
+    stepV(0),
     minPeriod(other.minPeriod),
     maxPeriod(other.maxPeriod),
     noiseConfig(other.noiseConfig->clone())
 {
     // Проходим по вектору умных указателей и копируем их
-    for (const auto & peakConfig : other.peakConfigs) {
-        peakConfigs.push_back(peakConfig->clone());
+    for (const auto &peakConfig : other.peakConfigsV) {
+        peakConfigsV.push_back(peakConfig->clone());
     }
 }
 
 LoadGenerator::PostConfig & LoadGenerator::PostConfig::operator=(const PostConfig &other) {
     if (this == &other)
         return *this;
+    enabled = other.enabled;
     postName = other.postName;
     latitude = other.latitude;
     longitude = other.longitude;
     frequency = other.frequency;
     level = other.level;
     levelSigma = other.levelSigma;
-    min_angle = other.min_angle;
-    max_angle = other.max_angle;
-    step = other.step;
+    minAngleH = other.minAngleH;
+    maxAngleH = other.maxAngleH;
+    minAngleV = other.minAngleV;
+    maxAngleV = other.maxAngleV;
+    stepH = other.stepH;
+    stepV = other.stepV;
     minPeriod = other.minPeriod;
     maxPeriod = other.maxPeriod;
     noiseConfig = other.noiseConfig->clone();
     // Проходим по вектору умных указателей и копируем их
-    for (const auto & peakConfig : other.peakConfigs) {
-        peakConfigs.push_back(peakConfig->clone());
+    for (const auto & peakConfig : other.peakConfigsV) {
+        peakConfigsV.push_back(peakConfig->clone());
     }
     return *this;
 }
 
 LoadGenerator::Post::Post(PostConfig const &config) :
-    _config(config)
+    _config(config),
+    _rng(std::random_device{}())
 {
     _noise = _config.noiseConfig->createNoise(); // В зависимости от типа конфига нам выдадут разные шумы
-    for (const auto & peakConfig : _config.peakConfigs) {
-        _peaks.push_back(peakConfig->createPeak()); // В зависимости от типа конфига нам выдадут разные пики
+    for (const auto & peakConfig : _config.peakConfigsV) {
+        _peaksV.push_back(peakConfig->createPeak()); // В зависимости от типа конфига нам выдадут разные пики
+    }
+    for (const auto & peakConfig : _config.peakConfigsH) {
+        _peaksH.push_back(peakConfig->createPeak()); // В зависимости от типа конфига нам выдадут разные пики
+    }
+
+    // Инициализвация вектора градусов (горизонтальных)
+
+    const double rangeH = _config.maxAngleH - _config.minAngleH;
+    const auto stepCountH = static_cast<std::size_t>(
+        std::floor(rangeH / _config.stepH)
+    );
+    for (size_t i = 0; i <= stepCountH; ++i ) {
+        _degH.push_back(static_cast<double>(i) * _config.stepH + _config.minAngleH);
+    }
+
+    // Инициализация вектора градусов (вертикальных)
+
+    const double rangeV = _config.maxAngleV - _config.minAngleV;
+    const auto stepCountV = static_cast<std::size_t>(
+        std::floor(rangeV / _config.stepV)
+    );
+    for (size_t i = 0; i <= stepCountV; ++i ) {
+        _degV.push_back(static_cast<double>(i) * _config.stepV + _config.minAngleV);
+    }
+
+}
+
+void LoadGenerator::Post::call(TimePoint const &now) {
+    if (_config.enabled) {  // Если пост включён
+        if (now >= _nextGenTime) {  // Если пришло время новой генерации
+
+            // Расчёт свёртки по горизонтали
+            _data.convH.clear();
+            for (const auto & deg : _degH) {
+                double valueH = _noise->next();
+                for (const auto & peak : _peaksH) {
+                    valueH += peak->valueAt(deg);
+                }
+                _data.convH.push_back(valueH);
+            }
+
+            // Расчёт свёртки по вертикали
+            _data.convV.clear();
+            for (const auto & deg : _degV) {
+                double valueV = _noise->next();
+                for (const auto & peak : _peaksV) {
+                    valueV += peak->valueAt(deg);
+                }
+                _data.convV.push_back(valueV);
+            }
+
+            // Нормировка горизонтальной свёртки и запись качества и направления
+            const auto maxItH = std::max_element(_data.convH.begin(), _data.convH.end());
+            const double maxValueH = *maxItH;
+            for (auto & value : _data.convH) {
+                value /= maxValueH;
+            }
+            _data.qualityH = maxValueH;   // Записываем это как качество IDK: Я не знаю как расчитать quality
+            const auto maxIndexH = std::distance(_data.convH.begin(), maxItH);
+            _data.bearingH = _degH[maxIndexH];   // Записываем направление по максимуму
+
+            // Нормировка вертикальной свёртки и запись качества и направления
+            const auto maxItV = std::max_element(_data.convV.begin(), _data.convV.end());
+            const double maxValueV = *maxItV;
+            for (auto & value : _data.convV) {
+                value /= maxValueV;
+            }
+            _data.qualityV = maxValueV;   // Записываем это как качество IDK: Я не знаю как расчитать quality
+            const auto maxIndexV = std::distance(_data.convV.begin(), maxItV);
+            _data.bearingV = _degH[maxIndexV];   // Записываем направление по максимуму
+
+            // Расчёт временной метки
+            _data.timestamp = QDateTime::currentDateTimeUtc();
+
+            // Далее копирование информации которую мы и так знаем
+
+            _data.level = _config.level;
+            _data.frequency = _config.frequency;
+            _data.coordinate.setLatitude(_config.latitude);
+            _data.coordinate.setLongitude(_config.longitude);
+            _data.postName = _config.postName;
+
+            _data.minAngleH = _config.minAngleH;
+            _data.maxAngleH = _config.maxAngleH;
+            _data.minAngleV = _config.minAngleV;
+            _data.maxAngleV = _config.maxAngleV;
+            _data.stepH = _config.stepH;
+            _data.stepV = _config.stepV;
+
+            newNextGenTime();   // Генерируем время, через которое произойдёт новая генерация
+
+            emit signalSendData(_data);     // Отправляем сигнал
+        }
     }
 }
+
+void LoadGenerator::Post::newNextGenTime()
+{
+    std::uniform_real_distribution<double> cooldownDistribution {
+        static_cast<double>(_config.minPeriod), static_cast<double>(_config.maxPeriod)
+    }; // Создаём закон равномерного распределения
+    const double cooldownSeconds = cooldownDistribution(_rng);  // Генерируем кд в секундах
+    const auto cooldownDuration = std::chrono::duration<double>(cooldownSeconds); // Кастим в duration
+
+    // Кастим в steady_clock::duration и складываем со steady_clock_now
+    _nextGenTime = Clock::now() + std::chrono::duration_cast<Clock::duration>(cooldownDuration);
+}
+
 
 LoadGenerator::LoadGenerator(std::vector<PostConfig> const &postConfigs, QObject *parent) :
     QObject(parent),
     _postConfigs(postConfigs)
 {
-
+    load();
 }
 
 std::vector<LoadGenerator::PostConfig> LoadGenerator::postConfigs() const
@@ -66,10 +185,12 @@ std::vector<LoadGenerator::PostConfig> LoadGenerator::postConfigs() const
 void LoadGenerator::setPostConfigs(const std::vector<PostConfig> &postConfigs)
 {
     _postConfigs = postConfigs;
+    load();
 }
 
 void LoadGenerator::load() {
+    _posts.clear();
     for (const auto & postConfig : _postConfigs) {
-        _posts.emplace_back(postConfig);
+        _posts.emplace_back(postConfig);    // Закидываем в вектор Postы, инициализованные postConfigом
     }
 }
--- Qt/myConvolution/loadgenerator.h
+++ Qt/myConvolution/loadgenerator.h
@@ -2,6 +2,8 @@
 
 #include <QObject>
 #include <QString>
+#include <QDateTime>
+#include <QtPositioning>
 #include <vector>
 
 #include "noise.h"
@@ -15,7 +17,6 @@ public:
     // Конфигурация поста
     struct PostConfig
     {
-        // TODO: Добавить угол места
         bool enabled;       // Генерирует ли пост
         QString postName;   // Имя поста
         double latitude;    // Широта (градусы)
@@ -24,30 +25,75 @@ public:
         double level;       // Амплитуда (дБ) (Гауссово распределение)
         double levelSigma;  // СКО амплитуды (Гауссово распределение)
 
-        int min_angle;               // Минимальный угол
-        int max_angle;               // Максимальный угол
-        double step;                 // Шаг
-        std::chrono::seconds minPeriod;      // Минимальный период между отправками
-        std::chrono::seconds maxPeriod;      // Максимальный период между отправками
+        int minAngleH;               // Минимальный угол (горизонтальный)
+        int maxAngleH;               // Максимальный угол (горизонтальный)
+        int minAngleV;               // Минимамльный угол (вертикальный)
+        int maxAngleV;               // Максимальный угол (вертикальный)
+        double stepH;                // Шаг (горизонтальный)
+        double stepV;                // Шаг (вертикальный)
+        unsigned int minPeriod;      // Минимальный период между отправками
+        unsigned int maxPeriod;      // Максимальный период между отправками
 
         std::unique_ptr<AbstractNoise::NoiseConfig> noiseConfig;                // Шум
-        std::vector<std::unique_ptr<AbstractPeak::PeakConfig>> peakConfigs;     // Пики
+        std::vector<std::unique_ptr<AbstractPeak::PeakConfig>> peakConfigsV;    // Пики по вертикали
+        std::vector<std::unique_ptr<AbstractPeak::PeakConfig>> peakConfigsH;   // Пики по горизонтали
 
         PostConfig(const PostConfig &other);
         PostConfig & operator=(const PostConfig &other);
     };
 
+    // Пакет данных
+    struct DataPackage
+    {
+        QDateTime timestamp;        // Временная метка
+        double bearingH;            // Направление по горизонтали
+        double bearingV;            // Направление по вертикали
+        double qualityH;            // Качество свёртки по горизонтали
+        double qualityV;            // Качество свёртки по вертикали
+        double level;               // Мощность
+        double frequency;           // Частота
+        QGeoCoordinate coordinate;  // Координата
+        QString postName;           // Имя поста
+
+        int minAngleH;               // Минимальный угол (горизонтальный)
+        int maxAngleH;               // Максимальный угол (горизонтальный)
+        int minAngleV;               // Минимамльный угол (вертикальный)
+        int maxAngleV;               // Максимальный угол (вертикальный)
+        double stepH;                // Шаг (горизонтальный)
+        double stepV;                // Шаг (вертикальный)
+
+        std::vector<double> convH;   // Свёртка по горизонтали
+        std::vector<double> convV;   // Свёртка по вертикали
+    };
+
     // Класс одного поста
     class Post
     {
+        using Clock = std::chrono::steady_clock;
+        using TimePoint = Clock::time_point;
+
     public:
         explicit Post(PostConfig const &config);
 
     private:
         PostConfig _config;
+        DataPackage _data;
+
+        std::mt19937 _rng;       // Движок случайной генерации
+        TimePoint _nextGenTime;     // Следующее время генерации
+        void newNextGenTime();  // Создать следующее время генерации
+
+        std::vector<double> _degH;  // Углы по горизонтали
+        std::vector<double> _degV;  // Углы по вертикали
 
         std::unique_ptr<AbstractNoise> _noise;              // Шум (мы не знаем что это за шум)
-        std::vector<std::unique_ptr<AbstractPeak>> _peaks;  // Вектор пиков (мы не знаем что это за пики)
+        std::vector<std::unique_ptr<AbstractPeak>> _peaksV; // Вектор пиков по вертикали (мы не знаем что это за пики)
+        std::vector<std::unique_ptr<AbstractPeak>> _peaksH; // Вектор пиков по горизонтали
+
+        // Генерация сигнала (если пришло время) и отправка пакета данных
+        void call(TimePoint const &now);
+    signals:
+        void signalSendData(DataPackage const &data);
     };
 
     // Конструктор, когда нет пиков
